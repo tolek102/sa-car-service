5c713635d560710fbd76d9dfef46a2fcfcf38d31
pawel.bachta <pawel.bachta@gmail.com>
2022-01-12 20:10:48 +0100

    init commit

---
+++ src/main/java/pl/springacademy/carservice/SaM3CarServiceApplication.java
@@ -0,0 +1,12 @@
+package pl.springacademy.carservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaM3CarServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaM3CarServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/springacademy/carservice/SaM3CarServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package pl.springacademy.carservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaM3CarServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
