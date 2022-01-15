72f4d7da080c8e1067f64958f26e548e9bfeb55e
pawel.bachta <pawel.bachta@gmail.com>
2022-01-15 20:08:25 +0100

    add xml as additional return type

--- src/main/java/pl/springacademy/carservice/controller/CarController.java
+++ src/main/java/pl/springacademy/carservice/controller/CarController.java
@@ -3,6 +3,7 @@ package pl.springacademy.carservice.controller;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -28,37 +30,44 @@ public class CarController {
 
     private final CarService carService;
 
-    @GetMapping
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @ResponseBody
     public ResponseEntity<List<Car>> getAllCars() {
         return carService.getAllCars();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping(path = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @ResponseBody
     public ResponseEntity<Car> getCarById(@PathVariable final int id) {
         return carService.getCarById(id);
     }
 
-    @GetMapping("/color")
+    @GetMapping(path = "/color", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @ResponseBody
     public ResponseEntity<List<Car>> getCarsByColor(@RequestParam final Color color) {
         return carService.getCarsByColor(color);
     }
 
-    @PostMapping
+    @PostMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @ResponseBody
     public ResponseEntity<Car> addCar(@Validated @RequestBody final Car car) {
         return carService.addCar(car);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping(path = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @ResponseBody
     public ResponseEntity<Car> addOrUpdateCar(@PathVariable final int id, @Validated @RequestBody final Car car) {
         return carService.addOrUpdateCar(id, car);
     }
 
-    @PatchMapping("/{id}")
+    @PatchMapping(path = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @ResponseBody
     public ResponseEntity<Car> updateCarById(@PathVariable final int id, @RequestBody final Map<Object, Object> fieldsToUpdate) {
         return carService.updateCarById(id, fieldsToUpdate);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping(path = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @ResponseBody
     public ResponseEntity<Car> deleteCarById(@PathVariable final int id) {
         return carService.deleteCarById(id);
     }
